a16ec2e7ea4d47fc68f815c7cf4a0e860f67dbc1
Markus Kunesch <m.kunesch@damtp.cam.ac.uk>
2016-06-15 15:47:54 +0100

    Modified the FourtOrderDerivatives code again to make it more flexible and added FORCOMPS loop

--- FABDriver/FourthOrderDerivatives.hpp
+++ FABDriver/FourthOrderDerivatives.hpp
@@ -103,48 +103,52 @@ struct FourthOrderDerivatives
     }
 
     template <class data_t>
+    void
+    add_advection(std::array<data_t, c_NUM>& out,idx_t<data_t> idx, const data_t& vec, const int direction) const
+    {
+        const int stride = m_driver.m_in_stride[direction];
+        const auto shift_positive = simd_compare_gt(vec, 0.0);
+
+        for (int i = 0; i < c_NUM; ++i)
+        {
+            const auto in = SIMDIFY<data_t>(m_driver.m_in_ptr[i]);
+            const data_t in_left = in[idx - stride];
+            const data_t in_centre = in[idx];
+            const data_t in_right = in[idx + stride];
+
+            data_t weight_0 = -2.50000000000000000000e-1;
+            data_t weight_1 = -8.33333333333333333333e-1;
+            data_t weight_2 = +1.50000000000000000000e+0;
+            data_t weight_3 = -5.00000000000000000000e-1;
+            data_t weight_4 = +8.33333333333333333333e-2;
+
+            data_t upwind;
+            upwind = vec * (  weight_0 * in_left
+                                    + weight_1 * in_centre
+                                    + weight_2 * in_right
+                                    + weight_3 * in[idx + 2*stride]
+                                    + weight_4 * in[idx + 3*stride]) / m_dx;
+
+            data_t downwind;
+            downwind = vec * (- weight_4 * in[idx - 3*stride]
+                                    - weight_3 * in[idx - 2*stride]
+                                    - weight_2 * in_left
+                                    - weight_1 * in_centre
+                                    - weight_0 * in_right) / m_dx;
+
+            out[i] += simd_conditional(shift_positive, upwind , downwind);
+        }
+    }
+
+    template <class data_t>
     std::array<data_t, c_NUM>
-    advection(idx_t<data_t> idx, tensor<1, data_t> vec) const
+    advection(idx_t<data_t> idx, const tensor<1, data_t>& vec) const
     {
         std::array<data_t, c_NUM> out = { };
-
         for (int dim = 0; dim < IDX_SPACEDIM; ++dim)
         {
-            const int stride = m_driver.m_in_stride[dim];
-            const auto shift_val = vec[dim];
-            const auto shift_positive = simd_compare_gt(shift_val, 0.0);
-
-            for (int i = 0; i < c_NUM; ++i)
-            {
-                const auto in = SIMDIFY<data_t>(m_driver.m_in_ptr[i]);
-                const data_t in_left = in[idx - stride];
-                const data_t in_centre = in[idx];
-                const data_t in_right = in[idx + stride];
-
-                data_t weight_0 = -2.50000000000000000000e-1;
-                data_t weight_1 = -8.33333333333333333333e-1;
-                data_t weight_2 = +1.50000000000000000000e+0;
-                data_t weight_3 = -5.00000000000000000000e-1;
-                data_t weight_4 = +8.33333333333333333333e-2;
-
-                data_t upwind;
-                upwind = shift_val * (  weight_0 * in_left
-                                      + weight_1 * in_centre
-                                      + weight_2 * in_right
-                                      + weight_3 * in[idx + 2*stride]
-                                      + weight_4 * in[idx + 3*stride]) / m_dx;
-
-                data_t downwind;
-                downwind = shift_val * (- weight_4 * in[idx - 3*stride]
-                                        - weight_3 * in[idx - 2*stride]
-                                        - weight_2 * in_left
-                                        - weight_1 * in_centre
-                                        - weight_0 * in_right) / m_dx;
-
-                out[i] += simd_conditional(shift_positive, upwind , downwind);
-            }
+            add_advection(out, idx, vec[dim], dim);
         }
-
         return out;
     }
 
