9636a5b206b0b8baf4b09038dae6fe9b4986b577
Markus Kunesch <m.kunesch@damtp.cam.ac.uk>
2016-09-16 15:29:17 +0100

    Updated EnforceTfA to the new vars_t type

--- CCZ4/EnforceTfA.hpp
+++ CCZ4/EnforceTfA.hpp
@@ -13,66 +13,64 @@
 class EnforceTfA
 {
 protected:
-   const FABDriverBase& m_driver;
+    const FABDriverBase& m_driver;
 
 public:
-   EnforceTfA(const FABDriverBase& driver) :
-      m_driver (driver)
-   {}
+    EnforceTfA(const FABDriverBase& driver) :
+        m_driver (driver)
+    {}
 
-   template <class data_t>
-   struct vars_t
-   {
-      tensor<2, data_t> h;
-      tensor<2, data_t> A;
+    template <class data_t>
+    struct vars_t : VarsBase<data_t>
+    {
+        using VarsBase<data_t>::m_assignment_ptrs; //Saves us some writing later
 
-      vars_t(){}
+        tensor<2, data_t> h;
+        tensor<2, data_t> A;
 
-      template <class arr_t>
-      vars_t(const arr_t& in)
-      {
-         h[0][0]  = in[c_h11];
-         h[0][1]  = in[c_h12];
-         h[0][2]  = in[c_h13];
-         h[1][1]  = in[c_h22];
-         h[1][2]  = in[c_h23];
-         h[2][2]  = in[c_h33];
+        vars_t()
+        {
+            m_assignment_ptrs[c_h11].push_back(&h[0][0]);
+            m_assignment_ptrs[c_h12].push_back(&h[0][1]);
+            m_assignment_ptrs[c_h12].push_back(&h[1][0]);
+            m_assignment_ptrs[c_h13].push_back(&h[0][2]);
+            m_assignment_ptrs[c_h13].push_back(&h[2][0]);
+            m_assignment_ptrs[c_h22].push_back(&h[1][1]);
+            m_assignment_ptrs[c_h23].push_back(&h[1][2]);
+            m_assignment_ptrs[c_h23].push_back(&h[2][1]);
+            m_assignment_ptrs[c_h33].push_back(&h[2][2]);
 
-         h[1][0] = h[0][1];
-         h[2][0] = h[0][2];
-         h[2][1] = h[1][2];
+            m_assignment_ptrs[c_A11].push_back(&A[0][0]);
+            m_assignment_ptrs[c_A12].push_back(&A[0][1]);
+            m_assignment_ptrs[c_A12].push_back(&A[1][0]);
+            m_assignment_ptrs[c_A13].push_back(&A[0][2]);
+            m_assignment_ptrs[c_A13].push_back(&A[2][0]);
+            m_assignment_ptrs[c_A22].push_back(&A[1][1]);
+            m_assignment_ptrs[c_A23].push_back(&A[1][2]);
+            m_assignment_ptrs[c_A23].push_back(&A[2][1]);
+            m_assignment_ptrs[c_A33].push_back(&A[2][2]);
+        };
+    };
 
-         A[0][0]  = in[c_A11];
-         A[0][1]  = in[c_A12];
-         A[0][2]  = in[c_A13];
-         A[1][1]  = in[c_A22];
-         A[1][2]  = in[c_A23];
-         A[2][2]  = in[c_A33];
+    template <class data_t>
+    void compute(int x, int y, int z)
+    {
+        idx_t<data_t> idx = m_driver.in_idx(x, y, z);
 
-         A[1][0] = A[0][1];
-         A[2][0] = A[0][2];
-         A[2][1] = A[1][2];
-      }
-   };
+        vars_t<data_t> vars;
+        m_driver.local_vars(vars,idx);
 
-   template <class data_t>
-   void compute(int x, int y, int z)
-   {
-      idx_t<data_t> idx = m_driver.in_idx(x, y, z);
+        auto h_UU = TensorAlgebra::compute_inverse(vars.h);
+        TensorAlgebra::make_trace_free(vars.A, vars.h, h_UU);
 
-      vars_t<data_t> vars = m_driver.local_vars(idx);
-
-      auto h_UU = TensorAlgebra::compute_inverse(vars.h);
-      TensorAlgebra::make_trace_free(vars.A, vars.h, h_UU);
-
-      idx_t<data_t> out_idx = m_driver.out_idx(x, y, z);
-      SIMDIFY<data_t>(m_driver.m_out_ptr[c_A11])[out_idx] = vars.A[0][0];
-      SIMDIFY<data_t>(m_driver.m_out_ptr[c_A12])[out_idx] = vars.A[0][1];
-      SIMDIFY<data_t>(m_driver.m_out_ptr[c_A13])[out_idx] = vars.A[0][2];
-      SIMDIFY<data_t>(m_driver.m_out_ptr[c_A22])[out_idx] = vars.A[1][1];
-      SIMDIFY<data_t>(m_driver.m_out_ptr[c_A23])[out_idx] = vars.A[1][2];
-      SIMDIFY<data_t>(m_driver.m_out_ptr[c_A33])[out_idx] = vars.A[2][2];
-   }
+        idx_t<data_t> out_idx = m_driver.out_idx(x, y, z);
+        SIMDIFY<data_t>(m_driver.m_out_ptr[c_A11])[out_idx] = vars.A[0][0];
+        SIMDIFY<data_t>(m_driver.m_out_ptr[c_A12])[out_idx] = vars.A[0][1];
+        SIMDIFY<data_t>(m_driver.m_out_ptr[c_A13])[out_idx] = vars.A[0][2];
+        SIMDIFY<data_t>(m_driver.m_out_ptr[c_A22])[out_idx] = vars.A[1][1];
+        SIMDIFY<data_t>(m_driver.m_out_ptr[c_A23])[out_idx] = vars.A[1][2];
+        SIMDIFY<data_t>(m_driver.m_out_ptr[c_A33])[out_idx] = vars.A[2][2];
+    }
 };
 
 #endif /* FIXTFA_HPP_ */
