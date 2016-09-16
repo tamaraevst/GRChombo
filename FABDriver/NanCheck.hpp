967fb914ccb62341246cf015eb9f924e4a2cc4b3
Markus Kunesch <m.kunesch@damtp.cam.ac.uk>
2016-09-16 15:29:58 +0100

    Updated the NanCheck with more information about the crash

--- FABDriver/NanCheck.hpp
+++ FABDriver/NanCheck.hpp
@@ -10,20 +10,35 @@ class NanCheck
 {
 protected:
    const FABDriverBase& m_driver;
+   const std::string m_error_info = "NanCheck";
+   const double m_max_abs = 1e20;
 
 public:
-   NanCheck(const FABDriverBase& driver) :
-      m_driver (driver)
-   {}
+   NanCheck(const FABDriverBase& driver) : m_driver (driver) {}
+
+   NanCheck(const FABDriverBase& driver, const std::string a_error_info) : m_driver (driver), m_error_info (a_error_info) {}
+
+   NanCheck(const FABDriverBase& driver, const std::string a_error_info, const double a_max_abs) :
+       m_driver (driver), m_error_info (a_error_info), m_max_abs (a_max_abs) {}
 
    void compute(int ix, int iy, int iz)
    {
       idx_t<double> idx = m_driver.in_idx(ix, iy, iz);
+      bool stop = false;
       FORVARS(i)
       {
           const double val = m_driver.local_vars(idx,i);
-          if ( isnan(val) )
-              MayDay::Error("values have become nan");
+          if ( isnan(val) || abs(val) > m_max_abs) stop = true;
+      }
+      if (stop)
+      {
+          pout() << m_error_info << "::Values have become nan. The current state is: " << endl;
+          FORVARS(i)
+          {
+              pout() << i << ": " << m_driver.local_vars(idx,i) << endl;
+          }
+          pout() << "ix: " << ix << " iy: " << iy << " iz: " << iz << endl;
+          MayDay::Error("Values have become nan.");
       }
    }
 };
