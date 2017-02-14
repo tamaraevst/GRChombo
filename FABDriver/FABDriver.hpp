a9837ae09fb8ef85e405b2b89f6645f7a8fbb6d3
Markus Kunesch <m.kunesch@damtp.cam.ac.uk>
2017-02-14 18:13:43 +0000

    Added no_simd_support to FABDriver and extracted inner loop to simplify the code. Fixes issue #6.

--- FABDriver/FABDriver.hpp
+++ FABDriver/FABDriver.hpp
@@ -27,9 +27,17 @@ public:
     template <typename... param_types>
     FABDriver(param_types... params);
 
+    ///Perform the innermost (i.e. the x) loop, standard version with simd
+    void innermost_loop(const int iy, const int iz, const int loop_lo_x, const int loop_hi_x);
+    ///Perform the innermost (i.e. the x) loop, switches simd off for compute classes which have simd support
+    void innermost_loop(const int iy, const int iz, const int loop_lo_x, const int loop_hi_x, disable_simd);
+    ///Perform the innermost (i.e. the x) loop, for compute classes without simd support
+    ///(i.e. where the compute function is not templated at all.
+    void innermost_loop(const int iy, const int iz, const int loop_lo_x, const int loop_hi_x, no_simd_support);
+
     //Takes input 'in', writes output into the subox 'loop_box' of 'out'
-    void execute(const FArrayBox& in, FArrayBox& out, const Box & loop_box);
-    void execute(const FArrayBox& in, FArrayBox& out, const Box & loop_box, disable_simd);
+    template <typename... simd_info>
+    void execute(const FArrayBox& in, FArrayBox& out, const Box & loop_box, simd_info... info);
 
     template <typename... simd_info>
     void execute(const FArrayBox& in, FArrayBox& out, simd_info... info); //Uses out.box() as loop_box
