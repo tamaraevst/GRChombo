31996120de2b429cd7ae3e55060b6235b8f95750
Markus Kunesch <m.kunesch@damtp.cam.ac.uk>
2018-01-18 10:20:35 +0000

    Allow the user to override the dimension definition from the command line.

--- Source/utils/DimensionDefinitions.hpp
+++ Source/utils/DimensionDefinitions.hpp
@@ -1,7 +1,12 @@
 #ifndef GRUTILS_HPP_
 #define GRUTILS_HPP_
 
+#ifndef GR_SPACEDIM
 #define GR_SPACEDIM 3
+#endif
+
+#ifndef DEFAULT_TENSOR_DIM
 #define DEFAULT_TENSOR_DIM 3
+#endif
 
 #endif /* GRUTILS_HPP_*/
