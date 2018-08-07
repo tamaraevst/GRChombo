b984fe0b16477ceacdc072f1630a09eddbeb6111
Miren Radia <miren_radia@yahoo.co.uk>
2018-08-07 16:20:29 +0100

    Modified ComplexScalarField.hpp

--- Source/Matter/ComplexScalarField.hpp
+++ Source/Matter/ComplexScalarField.hpp
@@ -7,7 +7,7 @@
 #define COMPLEXSCALARFIELD_HPP_
 
 #include "CCZ4Geometry.hpp"
-#include "DefaultPotential.hpp"
+#include "DefaultComplexPotential.hpp"
 #include "FourthOrderDerivatives.hpp"
 #include "Tensor.hpp"
 #include "TensorAlgebra.hpp"
@@ -16,19 +16,8 @@
 
 //!  Calculates the matter type specific elements such as the EMTensor and
 //   matter evolution
-/*!
-     This class is an example of a matter_t object which calculates the
-     matter type specific elements for the RHS update and the evaluation
-     of the constraints. This includes the Energy Momentum Tensor, and
-     the matter evolution terms. In this case, a scalar field,
-     the matter elements are phi and (minus) its conjugate momentum, Pi.
-     It is templated over a potential function potential_t which the
-     user must specify in a class, although a default is provided which
-     sets dVdphi and V_of_phi to zero.
-     It assumes minimal coupling of the field to gravity.
-     \sa MatterCCZ4(), ConstraintsMatter()
-*/
-template <class potential_t = DefaultPotential> class ComplexScalarField
+
+template <class potential_t = DefaultComplexPotential> class ComplexScalarField
 {
   protected:
     //! The local copy of the potential
@@ -41,23 +30,29 @@ template <class potential_t = DefaultPotential> class ComplexScalarField
     //! Structure containing the variables for the matter fields
     template <class data_t> struct CSFObject
     {
-        data_t phi;
-        data_t Pi;
+        data_t phi_Re;
+        data_t phi_Im;
+        data_t Pi_Re;
+        data_t Pi_Im;
     };
 
     //! Structure containing the rhs variables for the matter fields
     template <class data_t> struct Vars
     {
-        data_t phi;
-        data_t Pi;
+        data_t phi_Re;
+        data_t phi_Im;
+        data_t Pi_Re;
+        data_t Pi_Im;
 
         /// Defines the mapping between members of Vars and Chombo grid
         /// variables (enum in User_Variables)
         template <typename mapping_function_t>
         void enum_mapping(mapping_function_t mapping_function)
         {
-            VarsTools::define_enum_mapping(mapping_function, c_phi, phi);
-            VarsTools::define_enum_mapping(mapping_function, c_Pi, Pi);
+            VarsTools::define_enum_mapping(mapping_function, c_phi_Re, phi_Re);
+            VarsTools::define_enum_mapping(mapping_function, c_phi_Im, phi_Im);
+            VarsTools::define_enum_mapping(mapping_function, c_Pi_Re, Pi_Re);
+            VarsTools::define_enum_mapping(mapping_function, c_Pi_Im, Pi_Im);
         }
     };
 
@@ -65,14 +60,16 @@ template <class potential_t = DefaultPotential> class ComplexScalarField
     //!  2nd derivs
     template <class data_t> struct Diff2Vars
     {
-        data_t phi;
+        data_t phi_Re;
+        data_t phi_Im;
 
         /// Defines the mapping between members of Vars and Chombo grid
         ///  variables (enum in User_Variables)
         template <typename mapping_function_t>
         void enum_mapping(mapping_function_t mapping_function)
         {
-            VarsTools::define_enum_mapping(mapping_function, c_phi, phi);
+            VarsTools::define_enum_mapping(mapping_function, c_phi_Re, phi_Re);
+            VarsTools::define_enum_mapping(mapping_function, c_phi_Im, phi_Im);
         }
     };
 
@@ -92,9 +89,11 @@ template <class potential_t = DefaultPotential> class ComplexScalarField
     static void emtensor_excl_potential(
         emtensor_t<data_t> &out,         //!< the em tensor output
         const vars_t<data_t> &vars,      //!< the value of the variables
-        const SFObject<data_t> &vars_sf, //!< the value of the sf variables
+        const CSFObject<data_t> &vars_csf, //!< the value of the csf variables
         const Tensor<1, data_t>
-            &d1_phi,                   //!< the value of the first deriv of phi
+            &d1_phi_Re,             //!< the value of the first deriv of phi_Re
+        const Tensor<1, data_t>
+            &d1_phi_Im,             //!< the value of the first deriv of phi_Im
         const Tensor<2, data_t> &h_UU, //!< the inverse metric (raised indices).
         const Tensor<3, data_t>
             &chris_ULL); //!< the conformal christoffel symbol
@@ -116,16 +115,16 @@ template <class potential_t = DefaultPotential> class ComplexScalarField
     //! excluding the potential
     template <class data_t, template <typename> class vars_t>
     static void matter_rhs_excl_potential(
-        SFObject<data_t>
-            &rhs_sf, //!< the value of the RHS terms for the sf vars
+        CSFObject<data_t>
+            &rhs_csf, //!< the value of the RHS terms for the sf vars
         const vars_t<data_t> &vars,      //!< the values of all the variables
-        const SFObject<data_t> &vars_sf, //!< the value of the sf variables
+        const CSFObject<data_t> &vars_csf, //!< the value of the sf variables
         const vars_t<Tensor<1, data_t>> &d1, //!< the value of the 1st derivs
         const Tensor<1, data_t> &d1_phi, //!< the value of the 1st derivs of phi
         const Tensor<2, data_t> &d2_phi, //!< the value of the 2nd derivs of phi
-        const SFObject<data_t> &advec_sf); //!< advection terms for the sf vars
+        const CFObject<data_t> &advec_csf); //!< advection terms for the sf vars
 };
 
-#include "ScalarField.impl.hpp"
+#include "ComplexScalarField.impl.hpp"
 
 #endif /* COMPLEXSCALARFIELD_HPP_ */
