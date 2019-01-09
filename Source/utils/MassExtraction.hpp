147a6347565ffb99e6e92b14dc1e5e465c30308f
Miren Radia <miren_radia@yahoo.co.uk>
2019-01-09 16:08:49 +0000

    MassExtraction inherited from SphericalExtraction

---
+++ Source/utils/MassExtraction.hpp
@@ -0,0 +1,26 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef MASSEXTRACTION_HPP_
+#define MASSEXTRACTION_HPP_
+
+#include "SphericalExtraction.hpp"
+#include "UserVariables.hpp" // Needs c_Madm
+
+class MassExtraction : public SphericalExtraction
+{
+  public:
+    //! The constructor
+    MassExtraction(extraction_params_t a_params, double a_dt, double a_time)
+        : SphericalExtraction(c_Madm, a_params, a_dt, a_time) {}
+
+    //! Extract the mass
+    void execute_query(AMRInterpolator<Lagrange<4>> *a_interpolator) const
+    {
+        SphericalExtraction::execute_query(a_interpolator);
+        double Madm = SphericalExtraction::integrate_surface();
+        SphericalExtraction::write_integral(Madm, "ADMMass");
+    }
+}
