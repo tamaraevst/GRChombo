1f534e50f02bb8d08dfc2fbc3682e663b3b450ac
Miren Radia <miren_radia@yahoo.co.uk>
2019-01-10 16:34:40 +0000

    Corrected argument of CH_TIME.

--- Source/utils/MassExtraction.hpp
+++ Source/utils/MassExtraction.hpp
@@ -19,7 +19,7 @@ class MassExtraction : public SphericalExtraction
     //! Extract the mass
     void execute_query(AMRInterpolator<Lagrange<4>> *a_interpolator) const
     {
-        CH_TIME(MassExtraction::execute_query);
+        CH_TIME("MassExtraction::execute_query");
         SphericalExtraction::execute_query(a_interpolator);
         double Madm = SphericalExtraction::integrate_surface();
         SphericalExtraction::write_integral(Madm, "ADMmass.dat");
