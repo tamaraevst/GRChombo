b9e25175e1e6598d73c3b6a065580a0f2911f339
Robin Croft <rc634@damtp.cam.ac.uk>
2019-09-27 11:41:13 +0100

    inspiral stuff

--- Source/InitialConditions/BosonStars_R/BosonStar.impl.hpp
+++ Source/InitialConditions/BosonStars_R/BosonStar.impl.hpp
@@ -56,9 +56,9 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     double c_ = cosh(rapidity);
     double s_ = sinh(rapidity);
     double v_ = tanh(rapidity);
-    double t = coords.z*s_ + 0.*s_; //set /tilde{t} to zero
+    double t = coords.z*s_; //set /tilde{t} to zero
     double x = coords.x-separation/2.;
-    double z = coords.z*c_;
+    double z = coords.z*c_; //set /tilde{t} to zero
     double y = coords.y;
     double r = sqrt(x*x+y*y+z*z);
 
@@ -77,6 +77,7 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     double chi_;
     double phase_ = m_params_BosonStar.phase + w_*t;
     double beta_z = s_*c_*(psi_*psi_-omega_*omega_)/(pc_os);
+    vars.shift[2] += beta_z;
     double g_xx = psi_*psi_;
     double g_yy = psi_*psi_;
     double g_zz = pc_os;
@@ -88,7 +89,7 @@ void BosonStar::compute(Cell<data_t> current_cell) const
 
     double KLL[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
     double kLL[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
-    double K;
+    double K1, K2;
 
     kLL[0][0] = -lapse_1*s_*z*psi_prime_/(r*psi_);
     KLL[1][1] = KLL[0][0];
@@ -99,15 +100,15 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     KLL[0][1] = 0.;
     KLL[1][0] = 0.;
     KLL[2][2] = lapse_1*(z/r)*s_*c_*c_*(psi_prime_/psi_ - 2.*omega_prime_/omega_ + v_*v_*omega_*omega_prime_*pow(psi_,-2));
-    double thingy = omega_*psi_prime_*(2.*v_*v_*omega_*omega_-psi_*psi_) + omega_prime_*psi_*(v_*v_*omega_*omega_-2.*psi_*psi_);
-    K = pow(lapse_1/omega_,3)*pow(psi_,-5)*s_*c_*c_*(z/r)*(thingy);
+    double thing1 = omega_*psi_prime_*(2.*v_*v_*omega_*omega_-psi_*psi_) + omega_prime_*psi_*(v_*v_*omega_*omega_-2.*psi_*psi_);
+    K1 = pow(lapse_1/omega_,3)*pow(psi_,-5)*s_*c_*c_*(z/r)*(thingy1);
 
     if (binary)
     {
-        c_ = cosh(rapidity);
-        s_ = sinh(rapidity);
-        v_ = tanh(rapidity);
-        t = coords.z*s_ + 0.*s_; //set /tilde{t} to zero
+        c_ = cosh(-rapidity);
+        s_ = sinh(-rapidity);
+        v_ = tanh(-rapidity);
+        t = coords.z*s_; //set /tilde{t} to zero
         x = coords.x+separation/2.;
         z = coords.z*c_;
         y = coords.y;
@@ -126,6 +127,7 @@ void BosonStar::compute(Cell<data_t> current_cell) const
         w_ = m_1d_sol.get_w();
         phase_ = m_params_BosonStar.phase + w_*t;
         beta_z = s_*c_*(psi_*psi_-omega_*omega_)/(pc_os);
+        vars.shift[2] += beta_z;
         g_xx += psi_*psi_-1.;
         g_yy += psi_*psi_-1.;
         g_zz += pc_os-1.;
@@ -135,27 +137,42 @@ void BosonStar::compute(Cell<data_t> current_cell) const
         vars.Pi_Re += -(1./lapse_2)*( (x/r)*(s_-beta_z*c_)*dp_*cos(phase_) - w_*(c_-beta_z*s_)*p_*sin(phase_) );
         vars.Pi_Im += -(1./lapse_2)*( (x/r)*(s_-beta_z*c_)*dp_*sin(phase_) + w_*(c_-beta_z*s_)*p_*cos(phase_) );
 
+        kLL[0][0] += -lapse_2*s_*z*psi_prime_/(r*psi_);
+        KLL[1][1] += KLL[0][0];
+        KLL[2][1] += lapse_2*c_*s_*(y/r)*(psi_prime_/psi_ - omega_prime_/omega_ );
+        KLL[2][0] += lapse_2*c_*s_*(x/r)*(psi_prime_/psi_ - omega_prime_/omega_ );
+        KLL[1][2] += KLL[2][1];
+        KLL[0][2] += KLL[2][0];
+        KLL[0][1] += 0.;
+        KLL[1][0] += 0.;
+        KLL[2][2] += lapse_2*(z/r)*s_*c_*c_*(psi_prime_/psi_ - 2.*omega_prime_/omega_ + v_*v_*omega_*omega_prime_*pow(psi_,-2));
+        double thingy2 = omega_*psi_prime_*(2.*v_*v_*omega_*omega_-psi_*psi_) + omega_prime_*psi_*(v_*v_*omega_*omega_-2.*psi_*psi_);
+        K2 = pow(lapse_2/omega_,3)*pow(psi_,-5)*s_*c_*c_*(z/r)*(thingy2);
+
     }
     else if (BS_BH_binary)
     {
         double r_tilde = sqrt((x+separation)*(x+separation)+y*y+z*z + 10e-10);
-        double BH_lapse = (2.-M/r_tilde)/(2.+M/r_tilde);
+        double BH_gtt = (2.-M/r_tilde)/(2.+M/r_tilde);
         double BH_gii = pow(1.+M/(2.*r_tilde),4);
         g_xx += BH_gii-1.;
         g_yy += BH_gii-1.;
         g_zz += BH_gii-1.;
     }
 
-    vars.shift[2] += beta_z;
-    vars.chi += pow(g_xx*g_yy*g_zz,-1./3.);
-    vars.lapse += BS_BH_binary?sqrt(vars.chi):sqrt(lapse_1*lapse_1 + lapse_2+lapse_2-1.);
+
+    vars.chi = pow(g_xx*g_yy*g_zz,-1./3.);
+
+    if (BS_BH_binary){vars.lapse += sqrt(vars.chi);}
+    else if (binary){vars.lapse += sqrt(lapse_1*lapse_1 + lapse_2+lapse_2-1.);}
+    else{vars.lapse += lapse_1;}
     vars.h[0][0] += vars.chi*g_xx;
     vars.h[1][1] += vars.chi*g_yy;
     vars.h[2][2] += vars.chi*g_zz;
 
 
     double one_third = 1./3.;
-    vars.K += K;
+    vars.K += K1 + K2;
     vars.A[0][0] += chi_*(KLL[0][0]-one_third*K*g_xx);
     vars.A[1][1] += chi_*(KLL[1][1]-one_third*K*g_yy);
     vars.A[2][2] += chi_*(KLL[2][2]-one_third*K*g_zz);
