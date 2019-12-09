1cd3ec909ed807e19c14fd6d802064e19a33610b
Robin Croft <rc634@damtp.cam.ac.uk>
2019-12-09 15:59:02 +0000

    added phase diff

--- Source/InitialConditions/BosonStars_R/BosonStar.impl.hpp
+++ Source/InitialConditions/BosonStars_R/BosonStar.impl.hpp
@@ -74,7 +74,7 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     double lapse_1 = omega_*psi_/(sqrt(pc_os));
     double lapse_2 = 1.;
     double w_ = m_1d_sol.get_w();
-    double phase_ = m_params_BosonStar.phase + w_*t;
+    double phase_ = w_*t;
     double beta_z = s_*c_*(psi_*psi_-omega_*omega_)/(pc_os);
     vars.shift[2] += beta_z;
     double g_xx_1 = psi_*psi_;
@@ -143,7 +143,7 @@ void BosonStar::compute(Cell<data_t> current_cell) const
         pc_os = psi_*psi_*c_*c_ - omega_*omega_*s_*s_;
         lapse_2 = omega_*psi_/(sqrt(pc_os));
         w_ = m_1d_sol.get_w();
-        phase_ = m_params_BosonStar.phase + w_*t;
+        phase_ = m_params_BosonStar.phase*M_PI + w_*t;
         beta_z = s_*c_*(psi_*psi_-omega_*omega_)/(pc_os);
         vars.shift[2] += beta_z;
         g_xx_2 = psi_*psi_;
