b619b6ce954e400f90b80e9d3acf4ea3b1961d63
Robin Croft <rc634@damtp.cam.ac.uk>
2020-02-17 16:01:28 +0000

    abc

---
+++ Examples/BosonStar_R/SimulationParameters.hpp
@@ -0,0 +1,142 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef SIMULATIONPARAMETERS_HPP_
+#define SIMULATIONPARAMETERS_HPP_
+
+// General includes
+#include "GRParmParse.hpp"
+#include "SimulationParametersBase.hpp"
+
+// Problem specific includes:
+#include "ComplexPotential.hpp"
+#include "BosonStarParams.hpp"
+#include "GaussianFitTrackingParams.hpp"
+
+class SimulationParameters : public SimulationParametersBase
+{
+public:
+    SimulationParameters(GRParmParse &pp) : SimulationParametersBase(pp)
+    {
+        // Read the problem specific params
+        readParams(pp);
+    }
+
+    void readParams(GRParmParse &pp)
+    {
+        // for regridding
+        pp.load("regrid_threshold_phi", regrid_threshold_phi);
+        pp.load("regrid_threshold_chi", regrid_threshold_chi);
+
+        // Gravitional constant
+        pp.load("G_Newton", G_Newton, 1.0);
+
+        // Boson Star initial data params
+        pp.load("central_amplitude_CSF",
+                bosonstar_params.central_amplitude_CSF);
+        pp.load("phase", bosonstar_params.phase, 0.0);
+        pp.load("eigen", bosonstar_params.eigen, 0);
+        pp.load("gridpoints",bosonstar_params.gridpoints,400000);
+        pp.load("star_centre", bosonstar_params.star_centre,
+                {0.5 * L, 0.5 * L, 0.5 * L});
+
+        // Potential params
+        pp.load("scalar_mass", potential_params.scalar_mass, 1.0);
+        pp.load("phi4_coeff", potential_params.phi4_coeff, 0.0);
+        pp.load("solitonic", potential_params.solitonic, false);
+        pp.load("sigma_soliton", potential_params.sigma_soliton, 0.02);
+        pp.load("BS_binary", bosonstar_params.BS_binary, false);
+        pp.load("BS_BH_binary", bosonstar_params.BS_BH_binary, false);
+        pp.load("BlackHoleMass", bosonstar_params.BlackHoleMass, 0.);
+        pp.load("BS_rapidity", bosonstar_params.BS_rapidity, 0.0);
+        pp.load("BS_separation", bosonstar_params.BS_separation, 0.0);
+
+        // Mass extraction
+        pp.load("activate_mass_extraction", activate_mass_extraction, 0);
+        pp.load("num_mass_extraction_radii",
+                mass_extraction_params.num_extraction_radii, 1);
+        pp.load("mass_extraction_levels",
+                mass_extraction_params.extraction_levels,
+                mass_extraction_params.num_extraction_radii, 0);
+        pp.load("mass_extraction_radii",
+                mass_extraction_params.extraction_radii,
+                mass_extraction_params.num_extraction_radii, 0.1);
+        pp.load("num_points_phi_mass", mass_extraction_params.num_points_phi,
+                2);
+        pp.load("num_points_theta_mass",
+                mass_extraction_params.num_points_theta, 4);
+        pp.load("mass_extraction_center",
+                mass_extraction_params.extraction_center,
+                {0.5 * L, 0.5 * L, 0.5 * L});
+
+
+
+        // Star Tracking
+        pp.load("do_star_tracking", gaussfit_params.do_star_tracking, 0);
+        pp.load("num_points_gaussian_fit", gaussfit_params.num_points, 50);
+        // will be replaced
+        pp.load("tracked_field_index", gaussfit_params.field_index, 30);
+        pp.load("search_width", gaussfit_params.search_width, 16.);
+        pp.load("tracking_AMR_level", gaussfit_params.AMR_level,0);
+        pp.load("track_both_centres", gaussfit_params.track_both_centres, true);
+        pp.load("track_min_separation", gaussfit_params.track_min_separation, 5.);
+        pp.load("tracking_centre", gaussfit_params.track_centre,
+                {0.,0.,0.});
+        pp.load("tracking_centres", gaussfit_params.track_centres,
+                {0.,0.,0.,0.,0.,0.});
+
+
+
+        // Work out the minimum extraction level
+        auto min_extraction_level_it =
+            std::min_element(mass_extraction_params.extraction_levels.begin(),
+                             mass_extraction_params.extraction_levels.end());
+        mass_extraction_params.min_extraction_level = *(min_extraction_level_it);
+
+        // Do we cant to calculate L2 norms of constraint violations
+        pp.load("calculate_constraint_violations",
+                calculate_constraint_violations, false);
+
+        // Do we want to calculate and write the Noether Charge to a file
+        pp.load("calculate_noether_charge", calculate_noether_charge, false);
+
+        // Variables for outputting to plot files
+        pp.load("num_plot_vars", num_plot_vars, 0);
+        pp.load("plot_vars", plot_vars, num_plot_vars, 0);
+
+        // Variables for outputting inf-norm
+        pp.load("num_vars_inf_norm", num_vars_inf_norm, 0);
+        pp.load("vars_inf_norm", vars_inf_norm, num_vars_inf_norm, 0);
+    }
+
+    // Tagging thresholds
+    Real regrid_threshold_phi, regrid_threshold_chi;
+
+    // Initial data for matter and potential
+    double G_Newton;
+    BosonStar_params_t bosonstar_params;
+    Potential::params_t potential_params;
+    GaussFit_params_t gaussfit_params;
+
+    // Mass extraction
+    int activate_mass_extraction;
+    extraction_params_t mass_extraction_params;
+
+    // Do we want to write a file with the L2 norms of contraints?
+    bool calculate_constraint_violations;
+
+    // Do we want to write the Noether Charge to a file
+    bool calculate_noether_charge;
+
+    // Vars for outputting in plot files
+    int num_plot_vars;
+    std::vector<int> plot_vars;
+
+    // Vars for outputting inf-norms
+    int num_vars_inf_norm;
+    std::vector<int> vars_inf_norm;
+};
+
+#endif /* SIMULATIONPARAMETERS_HPP_ */
---
+++ Source/utils/GaussianFitTracking.hpp
@@ -0,0 +1,102 @@
+/* GRChombo
+ * Copyright 2012 The GRChombo collaboration.
+ * Please refer to LICENSE in GRChombo's root directory.
+ */
+
+#ifndef GAUSSIANFITTRACKING_HPP_
+#define GAUSSIANFITTRACKING_HPP_
+
+#include "AMRInterpolator.hpp"
+#include "InterpolationQuery.hpp"
+#include "Lagrange.hpp"
+#include "SmallDataIO.hpp" // for writing data
+#include "UserVariables.hpp" // Needs c_mod_phi etc
+#include "SimulationParametersBase.hpp"
+
+//////
+#include <fstream>
+#include <string>
+//////
+class GaussianFitTracking
+{
+    //add linking to parameter file for search width and shit
+    int m_number;
+    int m_field_index;
+    double m_delta, m_L;
+    double* m_vals;
+    double* m_array_x;
+    double* m_array_y;
+    double* m_array_z;
+    double* m_centre;
+    double* m_old_centre;
+    double m_dt, m_time, m_restart_time, m_min_separation;
+    double m_N; //number of coords we are tracking
+    bool m_first_step;
+    string m_filename = "StarTracking";
+    std::ofstream m_test_file;
+    GaussFit_params_t m_params_GaussFit;
+    bool star_positions_are_good = true;
+    int m_level;
+
+  public:
+
+
+    GaussianFitTracking(GaussFit_params_t a_params_GaussFit, double a_dt, double a_time,
+                                    double a_restart_time, bool a_first_step, double a_L, int a_level)
+                                    :m_params_GaussFit(a_params_GaussFit),m_dt(a_dt),
+                                    m_time(a_time),m_restart_time(a_restart_time),
+                                    m_first_step(a_first_step),m_L(a_L),m_level(a_level)
+    {
+        m_number = m_params_GaussFit.num_points;
+        m_field_index = m_params_GaussFit.field_index;
+        m_delta = m_params_GaussFit.search_width;
+        m_min_separation = m_params_GaussFit.track_min_separation;
+        m_test_file.open("TestDatFile.dat",std::ifstream::app);
+
+        if (m_params_GaussFit.track_both_centres)
+        {
+            m_N = 6;
+            m_vals = new double[6*m_number];
+            m_array_x = new double[6*m_number];
+            m_array_y = new double[6*m_number];
+            m_array_z = new double[6*m_number];
+            m_centre = new double[6];
+            m_old_centre = new double[6];
+        }
+        else
+        {
+            m_N = 3;
+            m_vals = new double[3*m_number];
+            m_array_x = new double[3*m_number];
+            m_array_y = new double[3*m_number];
+            m_array_z = new double[3*m_number];
+            m_centre = new double[3];
+            m_old_centre = new double[3];
+        }
+
+
+    }
+    ~GaussianFitTracking()
+    {
+        delete[] m_vals;
+        delete[] m_old_centre;
+        delete[] m_centre;
+        delete[] m_array_x;
+        delete[] m_array_y;
+        delete[] m_array_z;
+        m_test_file.close();
+    }
+    bool calculate_star_position_bool();
+    void get_data(AMRInterpolator<Lagrange<4>> *a_interpolator);
+    void get_data_2(AMRInterpolator<Lagrange<4>> *a_interpolator);
+    void find_centres();
+    void write_to_dat();
+    void write_to_dat_2();
+    void do_star_tracking(AMRInterpolator<Lagrange<4>> *a_interpolator);
+    void read_old_centre_from_dat();
+    void read_old_centre_from_dat_2()
+};
+
+#include "GaussianFitTracking.impl.hpp"
+
+#endif /* GAUSSIANFITTRACKING_HPP_ */
