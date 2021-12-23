d91a6d163686b0c08f164ebb19efbb41817b49c9
te307 <te307@cam.ac.uk>
2021-12-23 15:18:15 +0000

    comments

--- Source/InitialConditions/BosonStars_R/BosonStar.impl.hpp
+++ Source/InitialConditions/BosonStars_R/BosonStar.impl.hpp
@@ -45,11 +45,15 @@ void BosonStar::compute(Cell<data_t> current_cell) const
 {
     MatterCCZ4<ComplexScalarField<>>::Vars<data_t> vars;
     // Load variables (should be set to zero if this is a single BS)
+    
     current_cell.load_vars(vars);
     //VarsTools::assign(vars, 0.); // Set only the non-zero components below
+    
+    // Coordinates for centre of mass
     Coordinates<data_t> coords(current_cell, m_dx,
         m_params_BosonStar.star_centre);
 
+    // Import BS parameters and option of whether this is a BS binary or BS-BH binary
     double rapidity = m_params_BosonStar.BS_rapidity;
     double rapidity2 = m_params_BosonStar2.BS_rapidity;
     bool binary = m_params_BosonStar.BS_binary;
@@ -59,7 +63,12 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     double impact_parameter = m_params_BosonStar.BS_impact_parameter;
     double q = m_params_BosonStar.mass_ratio;
 
-    // boosts and coordinate objects
+    // Define boosts and coordinate objects, suppose star 1 is on the left of the centre of mass 
+    // and star 2 is on the right of centre of mass
+
+    //e.g. taking the centre of mass to be the origin, then STAR2 -------- (origin) -------- STAR1
+    
+    // First star positioning
     double c_ = cosh(rapidity);
     double s_ = sinh(rapidity);
     double v_ = tanh(rapidity);
@@ -69,7 +78,7 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     double y = coords.y+impact_parameter/2.;
     double r = sqrt(x*x+y*y+z*z);
 
-    // first star physical variables
+    // First star physical variables
     double p_ = m_1d_sol.get_p_interp(r);
     double dp_ = m_1d_sol.get_dp_interp(r);
     double omega_ = m_1d_sol.get_lapse_interp(r);
@@ -77,10 +86,13 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     double psi_ = m_1d_sol.get_psi_interp(r);
     double psi_prime_ = m_1d_sol.get_dpsi_interp(r);
 
+    // Get scalar field modulus, conformal factor, lapse and their gradients
     double pc_os = psi_*psi_*c_*c_ - omega_*omega_*s_*s_;
     double lapse_1 = omega_*psi_/(sqrt(pc_os));
     double lapse_2 = 1.;
     double w_ = m_1d_sol.get_w();
+
+    //Write in phase, shift, metric componnets of star 1 and initialise metric components of star 2
     double phase_ = w_*t;
     double beta_x = s_*c_*(psi_*psi_-omega_*omega_)/(pc_os);
     vars.shift[0] += beta_x;
@@ -89,11 +101,13 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     double g_xx_1 = pc_os;
     double g_xx_2=0., g_yy_2=0., g_zz_2=0., g_xx, g_yy, g_zz;
 
+    //Add on to evolution equations
     vars.phi_Re += p_*cos(phase_);
     vars.phi_Im += p_*sin(phase_);
     vars.Pi_Re += -(1./lapse_1)*( (x/r)*(s_-beta_x*c_)*dp_*cos(phase_) - w_*(c_-beta_x*s_)*p_*sin(phase_) );
     vars.Pi_Im += -(1./lapse_1)*( (x/r)*(s_-beta_x*c_)*dp_*sin(phase_) + w_*(c_-beta_x*s_)*p_*cos(phase_) );
 
+    //Initialise extrinsic curvature and metric with upper indices
     double KLL_1[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
     double KLL_2[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
     double KLL[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
@@ -102,6 +116,8 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     double gammaUU_1[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
     double gammaUU_2[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
     double K1=0., K2=0.;
+
+    // Fill them in
     gammaUU_1[0][0] = 1./g_xx_1;
     gammaUU_1[1][1] = 1./g_yy_1;
     gammaUU_1[2][2] = 1./g_zz_1;
@@ -117,9 +133,13 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     KLL_1[0][0] = lapse_1*(x/r)*s_*c_*c_*(psi_prime_/psi_ - 2.*omega_prime_/omega_ + v_*v_*omega_*omega_prime_*pow(psi_,-2));
     FOR2(i,j) K1 += gammaUU_1[i][j]*KLL_1[i][j];
 
-    // helfer fix variables
+     // Here we use Thomas Helfer's trick and find the corresponding fixed values to be substracted in the initial guess
     double helferLL[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
     double helferLL2[3][3] = {{0.,0.,0.},{0.,0.,0.},{0.,0.,0.}};
+
+    // Note that for equal mass helferLL = helferLL2
+
+     // This is the effect of object 1 on object 2 and hence represents the value to be substracted in the initial data 
     double t_p = (-separation)*s_; //set /tilde{t} to zero
     double x_p = (-separation)*c_;
     double z_p = 0.; //set /tilde{t} to zero
@@ -133,6 +153,8 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     double psi_prime_p = m_1d_sol.get_dpsi_interp(r_p);
     double pc_os_p = psi_p*psi_p*c_*c_ - omega_p*omega_p*s_*s_;
 
+    //Initialise weight function arguments to some random values - good check if in the compute
+    //of weight functions these values should never appear
     double arg1 = 42.0;
     double arg2 = 42.0;
 
@@ -148,11 +170,14 @@ void BosonStar::compute(Cell<data_t> current_cell) const
         std::cout << "h00 = " << h00_inf << ", h11 = " << h11_inf
                           << ", h22 = " << h22_inf << ", chi inf = " <<
                           chi_inf << std::endl;}*/
-        arg1 = (1.0/separation) * (sqrt(pow(coords.x+separation/(q+1), 2)+pow(coords.y,2)+pow(coords.z, 2)));
+
+        //Argument of weight function to be applied to to star 1
+        arg1 = (1.0/separation) * (sqrt(pow(coords.x-separation/(q+1), 2)+pow(coords.y,2)+pow(coords.z, 2)));
     }
 
     if (binary)
-    {
+    {   
+        //Second star positioning
         c_ = cosh(-rapidity2);
         s_ = sinh(-rapidity2);
         v_ = tanh(-rapidity2);
@@ -162,7 +187,7 @@ void BosonStar::compute(Cell<data_t> current_cell) const
         y = coords.y-impact_parameter/2.;
         r = sqrt(x*x+y*y+z*z);
 
-          // first star physical variables
+        //Ssecond star physical variables
         p_ = m_1d_sol2.get_p_interp(r);
         dp_ = m_1d_sol2.get_dp_interp(r);
         omega_ = m_1d_sol2.get_lapse_interp(r);
@@ -217,11 +242,29 @@ void BosonStar::compute(Cell<data_t> current_cell) const
         KLL_2[0][0] = lapse_2*(x/r)*s_*c_*c_*(psi_prime_/psi_ - 2.*omega_prime_/omega_ + v_*v_*omega_*omega_prime_*pow(psi_,-2));
         FOR2(i,j) K2 += gammaUU_2[i][j]*KLL_2[i][j];
 
-        helferLL2[1][1] = psi_p*psi_p;
-        helferLL2[2][2] = psi_p*psi_p;
-        helferLL2[0][0] = pc_os_p;
+        // Again, finding the values to be substracted from star 1
+        double t_p2 = (separation)*s_; //set /tilde{t} to zero
+        double x_p2 = (separation)*c_;
+        double z_p2 = 0.; //set /tilde{t} to zero
+        double y_p2 = -impact_parameter;
+        double r_p2 = sqrt(x_p2*x_p2+y_p2*y_p2+z_p2*z_p2);
 
-        arg2 = (1.0/separation) * (sqrt(pow(coords.x-separation*q/(q+1), 2)+pow(coords.y,2)+pow(coords.z,2)));
+        // Get physical variables needed for the metric
+        double p_p2 = m_1d_sol2.get_p_interp(r_p2);
+        double dp_p2 = m_1d_sol2.get_dp_interp(r_p2);
+        double omega_p2 = m_1d_sol2.get_lapse_interp(r_p2);
+        double omega_prime_p2 = m_1d_sol2.get_dlapse_interp(r_p2);
+        double psi_p2 = m_1d_sol2.get_psi_interp(r_p2);
+        double psi_prime_p2 = m_1d_sol2.get_dpsi_interp(r_p2);
+        double pc_os_p2 = psi_p2*psi_p2*c_*c_ - omega_p2*omega_p2*s_*s_;
+
+        //This is the effect of star 2 on star 1
+        helferLL2[1][1] = psi_p2*psi_p2;
+        helferLL2[2][2] = psi_p2*psi_p2;
+        helferLL2[0][0] = pc_os_p2;
+
+        //Argument of weight function to be applied to to star 2
+        arg2 = (1.0/separation) * (sqrt(pow(coords.x+separation*q/(q+1), 2)+pow(coords.y,2)+pow(coords.z,2)));
     }
     
     WeightFunction weight;
@@ -232,18 +275,19 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     weight1 = weight.compute_weight(arg1); // bump at object 1
     weight2 = weight.compute_weight(arg2); //bump at object 2
 
+    //Just some sanoty checks
     if (weight1 > 1.0)
     {DEBUG_OUT(weight1);}
 
     if (weight2 > 1.0)
     {DEBUG_OUT(weight2);}
-
     
     // Initial 3-metric 
-    g_xx = g_xx_1 + g_xx_2 - 1.0 - (weight1 * (helferLL[0][0] - 1.0) + weight2 * (helferLL2[0][0] - 1.0));
-    g_yy = g_yy_1 + g_yy_2 - 1.0 - (weight1 * (helferLL[1][1] - 1.0) + weight2 * (helferLL2[1][1] - 1.0));
-    g_zz = g_zz_1 + g_zz_2 - 1.0 - (weight1  * (helferLL[2][2] - 1.0) + weight2 * (helferLL2[2][2] - 1.0));
+    g_xx = g_xx_1 + g_xx_2 - 1.0 - (weight1 * (helferLL2[0][0] - 1.0) + weight2 * (helferLL[0][0] - 1.0));
+    g_yy = g_yy_1 + g_yy_2 - 1.0 - (weight1 * (helferLL2[1][1] - 1.0) + weight2 * (helferLL[1][1] - 1.0));
+    g_zz = g_zz_1 + g_zz_2 - 1.0 - (weight1  * (helferLL2[2][2] - 1.0) + weight2 * (helferLL[2][2] - 1.0));
 
+    // Now, compute upper and lower components
     gammaLL[0][0] = g_xx;
     gammaLL[1][1] = g_yy;
     gammaLL[2][2] = g_zz;
@@ -251,13 +295,16 @@ void BosonStar::compute(Cell<data_t> current_cell) const
     gammaUU[1][1] = 1./g_yy;
     gammaUU[2][2] = 1./g_zz;
 
+    // Define initial conformal factor
     double chi_ = pow(g_xx*g_yy*g_zz,-1./3.);
     vars.chi = chi_;
 
+    // Define initial lapse
     if (BS_BH_binary){vars.lapse += sqrt(vars.chi);}
     else if (binary){vars.lapse += sqrt(lapse_1*lapse_1 + lapse_2*lapse_2-1.);}
     else{vars.lapse += lapse_1;}
 
+    // Define initial trace of K and A_ij
     double one_third = 1./3.;
     FOR2(i,j) vars.h[i][j] = vars.chi*gammaLL[i][j];
     FOR4(i,j,k,l) KLL[i][j] += gammaLL[i][l]*(gammaUU_1[l][k]*KLL_1[k][j] + gammaUU_2[l][k]*KLL_2[k][j]);
