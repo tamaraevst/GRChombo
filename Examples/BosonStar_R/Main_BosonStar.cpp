bf90cb1488c5ea2bbca6f05c69dd3ad8796a9951
te307 <te307@cam.ac.uk>
2022-12-13 15:25:53 +0000

    further fixes to star tracking

--- Examples/BosonStar_R/Main_BosonStar.cpp
+++ Examples/BosonStar_R/Main_BosonStar.cpp
@@ -17,6 +17,9 @@
 // Problem specific includes:
 #include "BosonStarLevel.hpp"
 
+// Star tracking
+#include "STAMR.hpp"
+
 int runGRChombo(int argc, char *argv[])
 {
     // Load the parameter file and construct the SimulationParameter class
@@ -25,26 +28,32 @@ int runGRChombo(int argc, char *argv[])
     GRParmParse pp(argc - 2, argv + 2, NULL, in_file);
     SimulationParameters sim_params(pp);
 
+
     // The line below selects the problem that is simulated
     // (To simulate a different problem, define a new child of AMRLevel
     // and an associated LevelFactory)
-    GRAMR gr_amr;
-    DefaultLevelFactory<BosonStarLevel> boson_star_level_fact(gr_amr,
+    STAMR st_amr;
+
+    st_amr.m_star_tracker.initial_setup(
+        sim_params.star_track_centre, sim_params.do_star_track,
+        sim_params.number_of_stars, sim_params.initial_star_centres,
+        sim_params.star_track_resolution, sim_params.star_track_width);
+    DefaultLevelFactory<BosonStarLevel> boson_star_level_fact(st_amr,
                                                                   sim_params);
-    setupAMRObject(gr_amr, boson_star_level_fact);
+    setupAMRObject(st_amr, boson_star_level_fact);
 
     // Instantiate AMR interpolator for mass/GW extraction
     AMRInterpolator<Lagrange<4>> interpolator(
-                                       gr_amr, sim_params.origin, sim_params.dx, 
-                              sim_params.boundary_params, sim_params.verbosity);
-    gr_amr.set_interpolator(&interpolator);
+        st_amr, sim_params.origin, sim_params.dx, sim_params.boundary_params,
+        sim_params.verbosity);
+    st_amr.set_interpolator(&interpolator);
 
     // Add a scheduler to GRAMR which just calls doAnalysis on every AMRLevel
     // at time 0. It is called later in postTimeStep
     RefCountedPtr<CallDoAnalysis> call_do_analysis_ptr(new CallDoAnalysis);
     RefCountedPtr<Scheduler> scheduler_ptr(new Scheduler);
     scheduler_ptr->schedule(call_do_analysis_ptr, sim_params.max_steps);
-    gr_amr.schedule(scheduler_ptr);
+    st_amr.schedule(scheduler_ptr);
 
     using Clock = std::chrono::steady_clock;
     using Minutes = std::chrono::duration<double, std::ratio<60, 1>>;
@@ -52,13 +61,13 @@ int runGRChombo(int argc, char *argv[])
     std::chrono::time_point<Clock> start_time = Clock::now();
 
     // Engage! Run the evolution.
-    gr_amr.run(sim_params.stop_time, sim_params.max_steps);
+    st_amr.run(sim_params.stop_time, sim_params.max_steps);
 
     auto now = Clock::now();
     auto duration = std::chrono::duration_cast<Minutes>(now - start_time);
     pout() << "Total simulation time (mins): " << duration.count() << ".\n";
 
-    gr_amr.conclude();
+    st_amr.conclude();
 
     // Write Chombo timer report
     CH_TIMER_REPORT();
--- Examples/BosonStar_R/SimulationParameters.hpp
+++ Examples/BosonStar_R/SimulationParameters.hpp
@@ -92,6 +92,15 @@ public:
                 {0.,0.,0.});
         pp.load("tracking_centres", gaussfit_params.track_centres,
                 {0.,0.,0.,0.,0.,0.});
+        pp.load("star_track_centre", star_track_centre,
+                {0.5 * L, 0.5 * L, 0.5 * L});
+        pp.load("do_star_track", do_star_track, false);
+        pp.load("number_of_stars", number_of_stars, 1);
+        pp.load("initial_star_centres", initial_star_centres,
+                3 * number_of_stars);
+        pp.load("star_track_resolution", star_track_resolution, 21);
+        pp.load("star_track_width", star_track_width, 20.);
+        pp.load("star_track_level", star_track_level, 0);
 
         // Mass extraction
         pp.load("activate_mass_extraction", activate_mass_extraction, 0);
@@ -182,6 +191,13 @@ public:
     int num_vars_inf_norm;
     std::vector<int> vars_inf_norm;
 
+    std::array<double, CH_SPACEDIM> star_track_centre;
+    bool do_star_track;
+    int number_of_stars;
+    std::vector<double> initial_star_centres;
+    int star_track_resolution;
+    double star_track_width;
+    int star_track_level;
     int flux_extraction_level; // specifies times (level) to do angmom flux extraction
 };
 
