a5548f0ec8f9448552072c22ed8c0316aa6e6287
Tamara <86721223+tamaraevst@users.noreply.github.com>
2024-12-17 15:33:00 -0500

    update coefficients

--- Source/InitialConditions/RotatingBosonStar/RotatingBosonStar.impl.hpp
+++ Source/InitialConditions/RotatingBosonStar/RotatingBosonStar.impl.hpp
@@ -76,7 +76,7 @@ void RotatingBosonStar::compute(Cell<data_t> current_cell) const
     double beta_x = sin(theta)*sin(phi)*omega_val;
     double beta_y = -sin(theta)*cos(phi)*omega_val;
     double beta_z = 0;
-    double beta_phi = - omega_val/r; 
+    double beta_phi = -omega_val/r; 
 
     vars.shift[0] += beta_x;
     vars.shift[1] += beta_y;
@@ -133,13 +133,13 @@ void RotatingBosonStar::compute(Cell<data_t> current_cell) const
     //     pout() << "check02 is not 0 but " << check02 << endl;
     // }
 
-    KLL[0][0] = (l_val*sin(2.*phi)*(cos(theta) * sin(theta) * dthomega_val+sin(theta)*sin(theta)*(-omega_val + r*dromega_val)))/(4*f_val*r*lapse);
-    KLL[0][1] = -(l_val*cos(2.*phi)*(cos(theta) * sin(theta) * dthomega_val+sin(theta)*sin(theta)*(-omega_val + r*dromega_val)))/(4*f_val*r*lapse);
+    KLL[0][0] = (l_val*sin(2.*phi)*(cos(theta) * sin(theta) * dthomega_val+sin(theta)*sin(theta)*(-omega_val + r*dromega_val)))/(2*f_val*r*lapse);
+    KLL[0][1] = (l_val*cos(2.*phi)*(-cos(theta) * sin(theta) * dthomega_val+sin(theta)*sin(theta)*(omega_val - r*dromega_val)))/(2*f_val*r*lapse);
     KLL[1][0] = KLL[0][1];
     KLL[1][1] = -KLL[0][0];
-    KLL[0][2] = (-l_val*sin(phi)*(sin(theta)*sin(theta)*dthomega_val + cos(theta)*sin(theta)*(omega_val - r*dromega_val)))/(4.*f_val*r*lapse);
+    KLL[0][2] = (-l_val*sin(phi)*(sin(theta)*sin(theta)*dthomega_val + cos(theta)*sin(theta)*(omega_val - r*dromega_val)))/(2.*f_val*r*lapse);
     KLL[2][0] = KLL[0][2];
-    KLL[1][2] = (l_val*cos(phi)*(sin(theta)*sin(theta)*dthomega_val + cos(theta)*sin(theta)*(omega_val - r*dromega_val)))/(4.*f_val*r*lapse);
+    KLL[1][2] = (l_val*cos(phi)*(sin(theta)*sin(theta)*dthomega_val + cos(theta)*sin(theta)*(omega_val - r*dromega_val)))/(2.*f_val*r*lapse);
     KLL[2][1] = KLL[1][2];
     KLL[2][2] = 0;
 
