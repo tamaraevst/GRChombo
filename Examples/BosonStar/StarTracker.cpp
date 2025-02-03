4c6b2fdf9f0d6e6a48ee0f43647d75b68a277a91
Tamara Evstafyeva <dc-evst1@login7c.pri.cosma.local>
2025-02-03 01:05:21 +0000

    update star tracking

--- Examples/BosonStar/StarTracker.cpp
+++ Examples/BosonStar/StarTracker.cpp
@@ -350,28 +350,42 @@ void StarTracker::read_old_centre_from_dat(std::string a_filename, double a_dt,
                                            double a_time, double a_restart_time,
                                            bool a_first_step)
 {
-    std::vector<double> data_line;
-    SmallDataIO star_centre_file(a_filename, a_dt, a_time, a_restart_time,
-                                     SmallDataIO::READ, a_first_step);
     
+    int size;
+    std::vector<double> data_line;
+    std::vector<double> star_coords;
+
     if (a_time > a_dt / 3.)
     {
-    star_centre_file.get_specific_data_line(data_line, a_time - a_dt);
+        SmallDataIO star_centre_file(a_filename, a_dt, a_time, a_restart_time,
+                                     SmallDataIO::READ, a_first_step);
+        star_centre_file.get_specific_data_line(data_line, a_time - a_dt);
 
-    bool length_match = data_line.size() == m_num_stars * CH_SPACEDIM;
+        bool length_match = data_line.size() == m_num_stars * CH_SPACEDIM;
 
-    for (int i = 0; i < data_line.size(); i++)
-    {
-        m_star_coords[i] = data_line[i];
-    }
+        size = CH_SPACEDIM * m_num_stars;
+        star_coords.resize(size, 0);
 
-    pout() << "Star A restarted at : " << m_star_coords[0] << " " << m_star_coords[1] << " " << m_star_coords[2] << endl;
-    pout() << "Star B restarted at : " << m_star_coords[3] << " " << m_star_coords[4] << " " << m_star_coords[5] << endl;
-    }
-    else
-    {
-        MayDay::Error("Array size mismatch, when loading star positions "
+        if (length_match)
+        {
+            for (int i = 0; i < data_line.size(); i++)
+            {
+                star_coords[i] = data_line[i];
+            }
+
+            pout() << "Star A restarted at : " << star_coords[0] << " "
+                   << star_coords[1] << " " << star_coords[2] << endl;
+            pout() << "Star B restarted at : " << star_coords[3] << " "
+                   << star_coords[4] << " " << star_coords[5] << endl;
+        }
+        else
+        {
+            for (int i = 0; i < star_coords.size(); i++)
+            {
+                star_coords[i] = NAN;
+            }
+            MayDay::Error("Array size mismatch, when loading star positions "
                           "from StarCentres.dat file!");
+        }
     }
-
 }
